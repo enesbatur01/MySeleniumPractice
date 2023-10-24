eceda101861ce769578fdc9649c218941fd81917
Enes Batur <enay10171@gmail.com>
2023-10-24 23:14:19 +0300

    Selenium v1.2

--- src/main/java/Class.java
+++ src/main/java/Class.java
@@ -4,7 +4,7 @@ public class Class {
         String carNum = "1234 5678 9123 4567";
 
         String newCardNum = carNum.substring(0,carNum.length()-4)
-                .replaceAll("[0-9]","*")
+                .replaceAll("[0-9]","")
                 .concat(carNum.substring(carNum.length()-4));
 
         System.out.println(newCardNum);
